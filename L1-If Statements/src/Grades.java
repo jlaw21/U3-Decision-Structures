7a3a8b9d40365c7d6929a14356311676dc396419
pattojul000 <pattojul000@warren.k12.in.us>
2021-11-12 12:03:12 -0500

    ADD OR MULT

--- L1-If Statements/src/Grades.java
+++ L1-If Statements/src/Grades.java
@@ -10,25 +10,32 @@ public class Grades {
         double percentage = read.nextDouble();
 
         if(percentage < 60){
-            grade = "F";
+            System.out.println("Your grade is F");
         }
 
         if(percentage >= 60){
-            grade = "D";
+            if (percentage < 70) {
+                System.out.println("Your grade is D");
+            }
         }
 
         if(percentage >= 70){
-            grade = "C";
+            if (percentage < 80) {
+                System.out.println("Your grade is C");
+            }
         }
 
+
         if(percentage >= 80){
-            grade = "B";
+            if (percentage < 90) {
+                System.out.println("Your grade is B");
+            }
+
         }
 
         if(percentage >= 90){
-            grade = "A";
+            System.out.println("Your grade is A");
         }
 
-        System.out.println("Your grade is " + grade);
     }
 }
